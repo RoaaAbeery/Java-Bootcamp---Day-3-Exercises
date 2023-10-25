8d62ebd22cc39b78712184d878b10efe67fef0e6
RoaaAbeery <148685929+RoaaAbeery@users.noreply.github.com>
2023-10-25 08:34:45 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,116 @@
+import java.util.Random;
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+// Q1
+
+//        Scanner s1=new Scanner(System.in);
+//
+//        System.out.print("Enter the user role (admin, superuser, user): ");
+//        String role = s1.next();
+//
+//        if (role.equals("admin")) {
+//            System.out.println("Welcome admin");
+//        } else if (role.equals("superuser")) {
+//            System.out.println("Welcome superuser");
+//        } else if (role.equals("user")) {
+//            System.out.println("Welcome user");
+//        } else {
+//            System.out.println("Unknown role. Please enter 'admin', 'superuser',  'user'.");
+//        }
+//
+//        Q2
+//       Scanner s2=new Scanner (System.in);
+//        System.out.println("Input the 1st number:");
+//        int number1 = s2.nextInt();
+//        System.out.println("Input the 2st number:");
+//        int number2 = s2.nextInt();
+//        System.out.println("Input the 3st number:");
+//        int number3 = s2.nextInt();
+//
+//        if( number1 >= number2 && number1 >= number3)
+//            System.out.println( "The greatest:"+ number1 );
+//
+//        else if (number2 >= number1 && number2 >= number3)
+//            System.out.println(" The greatest:" + number2);
+//
+//        else
+//            System.out.println("The greatest:" + number3);
+
+//Q3
+
+//        Random random = new Random();
+//
+//        int randomNumber = random.nextInt(7) + 1;
+//
+//        System.out.println(randomNumber);
+//if (randomNumber ==1 ){
+//    System.out.println("sunday");
+//}
+// else if (randomNumber ==2) {
+//    System.out.println("monday");
+//        }
+//else if (randomNumber ==3) {
+//    System.out.println("tuesday");
+//}
+//else if (randomNumber ==4) {
+//    System.out.println("wednesday");
+//}
+//else if (randomNumber ==5) {
+//    System.out.println("thursday");
+//} else if (randomNumber ==6) {
+//    System.out.println("friday");
+//} else {
+//    System.out.println("saturday");
+//}
+
+//Q4
+
+//        Scanner s4 =new Scanner(System.in);
+//        System.out.println("Enter your numeric score:");
+//        int score=s4.nextInt();
+//
+//        if (score >= 90 && score <= 100) {
+//            System.out.println("Numeric Score: " + score);
+//            System.out.println("Letter Grade: A");
+//
+//        } else if (score >= 80 && score <= 89) {
+//            System.out.println("Numeric Score: " + score);
+//            System.out.println("Letter Grade: B");
+//
+//        } else if (score >= 70 && score <= 79) {
+//            System.out.println("Numeric Score: " + score);
+//            System.out.println("Letter Grade: C");
+//
+//
+//        } else if (score >= 60 && score <= 69) {
+//            System.out.println("Numeric Score: " + score);
+//            System.out.println("Letter Grade: D");
+//
+//        } else if (score >= 0 && score <= 59) {
+//            System.out.println("Numeric Score: " + score);
+//            System.out.println("Letter Grade: F");
+//
+//        } else {
+//            System.out.println("invalid");
+//        }
+
+//Q5
+//
+//Scanner s5=new Scanner(System.in);
+//        System.out.println("Enter your age: ");
+//int age =s5.nextInt();
+//if(age<13){
+//    System.out.println("You are a Child");
+//} else if (age >= 13 && age <= 19){
+//    System.out.println("You are a Teenager.");
+//}
+//else {
+//    System.out.println("You are an Adult");
+//}
+
+    }
+    }
